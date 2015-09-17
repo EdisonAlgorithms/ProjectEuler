0263eb5a96f610b5a2e77d11ad26e892d78c9eda
zeyuanxy <zeyuanxy@gmail.com>
2015-09-17 18:39:12 -0400

    add 191

---
+++ vol4/191.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    lst = [1, 3, 0, 2, 1, 0, 0, 1]
+    while lst[0] < 30:
+        n, t, a, b, c, d, e, f = lst
+        lst = [n + 1, 2 * t + b - a, c, 2 * b - a + d, t - (a + c), e, f, t]
+    print lst[1]
